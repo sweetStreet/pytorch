b13cf4b015f511d7d982a9541d4678235138d5ea
Rachel Guo <guorachel@meta.com>
2025-03-24 15:57:43 -0700

    save h100a combo kernel bench results
    Differential Revision: D71430452

--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -575,9 +575,9 @@ compute_all_bounds = False
 
 # enable the combo kernel that combines data-independent kernels (additional
 # to foreach kernels) into a single one (Experimental)
-combo_kernels = False
+combo_kernels = True
 # benchmark combo kernels and only allow ones with perf gains
-benchmark_combo_kernel = False
+benchmark_combo_kernel = True
 # combo_kernel autotuning options: 0 - disable, 1 - enable except for foreach,
 # 2 - enable for all
 combo_kernels_autotune = 1
