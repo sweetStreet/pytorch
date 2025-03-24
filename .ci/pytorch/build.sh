8056efbd6dbbc5749f8eaa195166fd16dabd9add
Catherine Lee <csl@fb.com>
2025-03-24 13:03:48 -0700

    tc

--- .ci/pytorch/build.sh
+++ .ci/pytorch/build.sh
@@ -27,6 +27,12 @@ cmake --version
 echo "Environment variables:"
 env
 
+if [[ ${CUDA_VERSION:0:2} == "12" ]]; then
+    pip install nvidia-nccl-cu12
+elif [[ ${CUDA_VERSION:0:2} == "11" ]]; then
+    pip install nvidia-nccl-cu11
+fi
+
 if [[ "$BUILD_ENVIRONMENT" == *cuda* ]]; then
   # Use jemalloc during compilation to mitigate https://github.com/pytorch/pytorch/issues/116289
   export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2
