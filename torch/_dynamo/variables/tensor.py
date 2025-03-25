ee2631425ec667efa6433be80e5925243f63b458
Ryan Guo <ryanguo99@meta.com>
2025-03-24 18:37:36 -0700

    [dynamo] Support `torch.Tensor._make_subclass` and tracing through tensor subclass `__new__`
    This builds off the previous patch in the stack, and fully fixes
    https://github.com/huggingface/diffusers/issues/10795.
    
    Essentially, tensor subclass in the issue uses
    `torch.Tensor._make_subclass`, which has a pretty simple shallow-copy
    plus type change semantics, as far as Dynamo is concerned. So this patch
    adds a polyfill for it.
    
    As a result, this allows us to trace through many user-defined `__new__`
    in tensor subclass (it's similar to how we trace through user-defined
    `__new__` for `UserDefinedClassVariable`), so this patch also faithfully
    trace through these `__new__` methods.
    
    ghstack-source-id: 2b10717843db5379932670a7465259427935d93e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/149483

--- torch/_dynamo/variables/tensor.py
+++ torch/_dynamo/variables/tensor.py
@@ -791,6 +791,9 @@ class TensorVariable(VariableTracker):
             return TensorWithTFOverrideVariable.from_tensor_var(
                 tx, self, py_cls, cls.source
             )
+        unimplemented(
+            "Argument of `as_subclass` must be a tensor subclass which Dynamo supports tracing"
+        )
 
     def method_get_device(self):
         if isinstance(self.device, torch.device):
@@ -1442,27 +1445,45 @@ class TensorSubclassVariable(VariableTracker):
         args: list[VariableTracker],
         kwargs: dict[str, VariableTracker],
     ) -> VariableTracker:
-        # Handle `Subclass(existing_tensor)` calls.
-        if len(args) == 1 and isinstance(args[0], TensorVariable):
-            from .torch_function import TensorWithTFOverrideVariable
+        from .torch_function import TensorWithTFOverrideVariable
 
-            # This simulates `__new__` and _assumes_ it doesn't have
-            # side-effects that matters to Dynamo tracing. TODO trace through
-            # `__new__`.
-            var = TensorWithTFOverrideVariable.from_tensor_var(
-                tx, args[0], self.value, self.source
+        data = args[0]
+        new_func = self.value.__new__
+        if new_func is torch.Tensor.__new__:
+            if (
+                len(args) == 1
+                and isinstance(args[0], TensorVariable)
+                and len(kwargs) == 0
+            ):
+                # Simulate `torch.Tensor.__new__` as shallow-copying the input
+                # tensor data with a new type. TODO polyfill?
+                var = TensorWithTFOverrideVariable.from_tensor_var(
+                    tx, data, self.value, self.source
+                )
+            else:
+                unimplemented_v2(
+                    gb_type="Calling subclass default constructor with more than tensor argument",
+                    context=f"{self.value}(args={args}, kwargs={kwargs})",
+                    explanation="Currently not supported",
+                    hints=[
+                        "Avoid this constructor call or move it outside "
+                        "`torch.compile` regione",
+                        *graph_break_hints.SUPPORTABLE,
+                    ],
+                )
+        else:
+            # Let Dynamo trace through custom `__new__`
+            var = VariableTracker.build(tx, new_func).call_function(
+                tx, [self] + args, kwargs
             )
 
-            # Let Dynamo trace through custom `__init__`
-            init_func = self.value.__init__
-            # TODO builder should be able to handle `torch.Tensor.__init__`,
-            # which is `object.__init__`, so that we can remove this check.
-            if init_func is not torch.Tensor.__init__:
-                cls_kwargs = kwargs or {}
-                VariableTracker.build(tx, init_func).call_function(
-                    tx, [var], cls_kwargs
-                )
-            return var
+        # Let Dynamo trace through custom `__init__`
+        init_func = self.value.__init__
+        # TODO builder should be able to handle `torch.Tensor.__init__`,
+        # which is `object.__init__`, so that we can remove this check.
+        if init_func is not torch.Tensor.__init__:
+            VariableTracker.build(tx, init_func).call_function(tx, [var], kwargs)
+        return var
 
         return super().call_function(tx, args, kwargs)
 
